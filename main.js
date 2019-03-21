db0e5c30b44de41339d05dc56830615bdbd7bf56
Mamas159 <gevogame@gmail.com>
2019-03-21 12:58:08 -0700

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ const bot = new RhythmBot({
         symbol: '-' // command symbol trigger
     },
     discord: {
-        token: '<NTU1NzE1ODA3NjExNDUzNDQy.D3V9kQ.MlKLKp_3pRxUZ2qCQ9ZXrwifRdw>'//,
+        token: 'NTU1NzE1ODA3NjExNDUzNDQy.D3V9kQ.MlKLKp_3pRxUZ2qCQ9ZXrwifRdw'//,
         // manage: {
         //     channels: [ 
         //         // Example text channel manager, limits media channel to 5 posts, limit should probably be less than 100 to avoid bugs
